66eb986127a64ca8f21c3c50d4c76f5fc0f9add2
Danny5577 <Dannysamuel5577@gmail.com>
2026-05-22 11:12:20 +0530

    added valid anagram

---
+++ projects/valid_anagram.py
@@ -0,0 +1,14 @@
+def anagram(s,t):
+    if len(s) != len(t):
+        return False
+    count_s={}
+    count_t={}
+    for char in s:
+        count_s[char]=count_s.get(char,0)+1
+    for char in t:
+        count_t[char]=count_t.get(char,0)+1
+    return count_s == count_t
+s="danny"
+t="yannd"
+a=anagram(s,t)
+print(a)
